f5247b402c0abb18a64665c53a2ce111e54608af
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-10-15 23:10:41 +0300

    added the error page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { HttpClient } from '@angular/common/http';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    NotFoundComponent,
+    
   ],
   imports: [
     BrowserModule,
---
+++ src/app/github-http/github-request.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Form } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubRequestService {
+ 
+  constructor() { }
+}
+
