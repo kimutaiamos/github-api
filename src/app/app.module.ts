06677acad64a4e0109203b6826b2ef7d5b5afc88
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-10-15 23:36:24 +0300

    added changes to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { HttpClient } from '@angular/common/http';
 import { NotFoundComponent } from './not-found/not-found.component';
-
+import { Routes } from '@angular/router';
+const routes: Routes = [
+  { path:'**', component:NotFoundComponent},
+  {path :'' ,redirectTo:"/form", pathMatch:"full"},
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,9 +20,11 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
+
   ],
-  providers: [],
+  providers: [RouterModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
